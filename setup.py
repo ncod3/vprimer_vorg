91a866040351244a9fdef86cac9a37c72592bc5d
ncod3 <ncod3@sunny-side-st.com>
2020-11-30 14:09:00 +0900

    2020.1130.1409

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name="vprimer",
-    version="0.0.1",
+    version="0.0.3",
     author="satoshi-natsume",
     author_email="s-natsume@ibrc.or.jp",
     description="Vprimer",

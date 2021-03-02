5deeafa0590923c8f43c090e878641e3e01ab3b6
ncod3 <ncod3@sunny-side-st.com>
2021-03-03 08:38:38 +0900

    2021.0303.0838

--- setup.py
+++ setup.py
@@ -4,14 +4,14 @@ with open("README.md", "r") as fh:
     long_description = fh.read()
 
 setuptools.setup(
-    name="vprimer",
+    name="vprimer_vorg",
     version="0.0.3",
     author="satoshi-natsume",
     author_email="s-natsume@ibrc.or.jp",
-    description="Vprimer",
+    description="Vprimer_vorg",
     long_description=long_description,
     long_description_content_type="text/markdown",
-    url="https://github.com/ncod3/vprimer",
+    url="https://github.com/ncod3/vprimer_vorg",
     packages=setuptools.find_packages(),
     license='GPL',
     entry_points = {
